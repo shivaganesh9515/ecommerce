7d4c3078b13a7936e7e754c6af3c7d9aeb4f2dfd
srikant <dhondi.srikanth@gmail.com>
2025-07-04 17:44:50 +0530

    Added checkout page

--- src/app/pages/checkout/checkout.ts
+++ src/app/pages/checkout/checkout.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-checkout',
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './checkout.html',
   styleUrl: './checkout.scss'
 })
